aca8dd0272f7ccc0f8b4e51d5d41b687555866f6
tbeiko <t.beiko23@gmail.com>
2018-01-27 15:45:56 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Candy Crisis
+Semester project for COMP472 
+
+The main file for this project is `candycrisis.py`
+
+### First deliverables (Feb. 15)
+Manual entries + Functionality of the rules of the game
+(e.g. visual trace after each move, detection of illegal moves, programming quality…) 
+
+## Game description 
+#### Input:
+Your program will read a series of initial configurations from a text file, where each line will represent an initial configuration. Each candy type will be represented by a unique character (‘r’, ‘b’, ‘w’, ‘y’, ‘g’ or ‘p’) and the empty slot will be indicated by the character ‘e’. For example, the line:
+e r r r r r b w b b w y b r y represents the first puzzle given on the previous page
+(empty at position A, Reese’s Pieces at positions B, C, D, E, F,
+a Bazooka Bubble Gum at G, a Walnetto at H, ...)
+
+#### Play Modes
+Your program should be able to run in manual mode and in automatic mode. This means that you should be able to run your program with:
+1. Manual entry for the player, i.e. a human indicating the moves by hand.
+  Note that if a human enters an illegal move, your program should give a warning and allow the user to enter a new move.
+2. Automatic mode, i.e. your “AI” solving and indicating the moves.
+  Note that if your program generates an illegal move, the entire puzzle solution will be considered wrong.
+
+After each move, your program must display the new configuration of the candy box.
+
+#### Output:
+Your program must output:
+1- A set of puzzle solutions: Your program must produce an output file, which contains, for each puzzle of the input file:
+a) the sequence of moves necessary to solve the puzzle,
+b) the time (real time in milliseconds in the lab’s desktops) required to solve the puzzle
+c) In addition, at the end of the output file, you must indicate:
+d) the total number of moves it took to solve all the puzzles of the input file
+2- A visual trace: In addition to the puzzle solutions, for each puzzle, your program must also display on the screen (or
+in another output file) the configuration of the candy box after each move. 
---
+++ candycrisis.py
@@ -0,0 +1,77 @@
+import numpy as np 
+import os
+
+INITIAL_GAME_CONFIGS = []
+output_file = 'outputs.txt'
+LETTER_BOARD = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
+
+def readConfigs(inputFile): 
+    inputs = open(inputFile, 'r')
+    for line in inputs:
+        INITIAL_GAME_CONFIGS.append(line)
+
+def printBoard(currentGame):
+    for i in range(0,15):
+        if i % 5 == 0:
+            print "\n|",
+        if currentGame[i] == 'e': 
+            print "  |",
+        else:
+            print currentGame[i] + " |",
+
+# To be implemented
+def validMove(move):
+    return True
+
+
+def main(): 
+    # To clear the screen 
+    print(chr(27) + "[2J")
+    print "Welcome to Candy Crisis\n";
+    print "Currently, only the manual mode is available, but come back soon to witness the automated mode!"
+    raw_input("Press Enter to continue...")
+    print(chr(27) + "[2J")
+
+    print "To choose your next move, simply type the letter corresponding to the candy you want to move in the empty space."
+    print "A board showing the letters for each position will be displayed underneath the game board."
+    print "Be quick! You are timed!"
+    raw_input("Press Enter to continue...")
+    print(chr(27) + "[2J")
+
+    # Get initial configurations from the input file 
+    readConfigs('inputs.txt') 
+    # Iterate over all games in file
+    gameCount = 1
+    for gameConfig in INITIAL_GAME_CONFIGS:
+        print "Game: " + str(gameCount)
+        currentGame = gameConfig.split()
+        printBoard(currentGame) 
+        print 
+        printBoard(LETTER_BOARD)
+        print
+
+        while True: 
+            move = raw_input("Enter a letter between A and O: ")
+
+            # String too long or too short 
+            if len(move) != 1: 
+                continue    
+            # Valid character  
+            elif ord(move.upper()) >= 65 and ord(move.upper()) < 80:
+                if validMove(move):
+                    break 
+            # Invalid character
+            else:
+                print "\nThat is not a letter between A and O. Try again."
+
+        # Clear screen  
+        print(chr(27) + "[2J")
+
+        # End of current game 
+        print "\n"
+        gameCount += 1
+
+    print "Thank you for playing!"
+
+if __name__ == '__main__':
+    main()
