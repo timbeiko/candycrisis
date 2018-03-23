f3c0fa7c386e65aa2bb2ae6f318072f6843a9c6a
Tim Beiko <t.beiko23@gmail.com>
2018-03-22 20:38:05 -0400

    Change input files name

--- generate_configs.py
+++ generate_configs.py
@@ -11,28 +11,28 @@ rs(EXPERT)
 rs(MASTER)
 
 
-with open("novice_inputs.txt", "w") as f:
+with open("inputs1.txt", "w") as f:
     for i in range(50):
         rs(NOVICE)
         for char in NOVICE:
             f.write(char + " ")
         f.write("\n")
 
-with open("apprentice_inputs.txt", "w") as f:
+with open("inputs2.txt", "w") as f:
     for i in range(50):
         rs(APPRENTICE)
         for char in APPRENTICE:
             f.write(char + " ")
         f.write("\n")
 
-with open("expert_inputs.txt", "w") as f:
+with open("inputs3.txt", "w") as f:
     for i in range(30):
         rs(EXPERT)
         for char in EXPERT:
             f.write(char + " ")
         f.write("\n")
 
-with open("master_inputs.txt", "w") as f:
+with open("inputs4.txt", "w") as f:
     for i in range(10):
         rs(MASTER)
         for char in MASTER:
